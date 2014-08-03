3d65f9a4a76d77160eca596cb6bcdf4e37d938b4
Zulfa Juniadi Zulkifli <zulfajuniadi@gmail.com>
2014-08-04 01:50:18 +0800

    uploads trait

---
+++ app/assets/javascripts/upload.js
@@ -0,0 +1,42 @@
+/**
+ *= require dropzone/downloads/dropzone
+ *= require_self
+ */
+
+Dropzone.autoDiscover = false;
+window.onload = function(){
+  var target = $("div#uploader");
+  var options = target.data();
+  var uploadOptions = { 
+    headers: {
+      'X-CSRF-Token': $('meta[name="_token"]').attr('content'),
+      'X-Uploader-Class' : options.class,
+      'X-Uploader-Id' : options.id,
+    },
+    url: '/uploader',
+    addRemoveLinks: true,
+  };
+  if(options.size) 
+    uploadOptions.maxFilesize = options.size;
+  if(options.files)
+    uploadOptions.maxFiles = options.files;
+  if(options.type)
+    uploadOptions.acceptedFiles = options.type;
+  var dz = new Dropzone(target[0], uploadOptions);
+
+  dz.on('success', function(file, response){
+    file.response = response;
+  });
+
+  dz.on('removedfile', function(file){
+    if(file.response) {
+      $.ajax({
+        method: 'DELETE',
+        contentType: "application/json",
+        dataType: "json",
+        url: '/uploader/' + file.response.id 
+      })  
+    }
+  });
+
+}
