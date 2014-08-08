ee43eb3fa71ed48a2c694b27854ad9771cce576f
Zulfa Juniadi Zulkifli <zulfajuniadi@gmail.com>
2014-08-08 12:34:03 +0800

    thumbnailable

---
+++ app/services/ThumbnailService.php
@@ -0,0 +1,103 @@
+<?php
+
+class ThumbnailService
+{
+    private $size;
+    private $exif;
+    private $path;
+    private $image;
+    private $widths = [
+        '_lg' => 1200,
+        '_md' => 800,
+        '_sm' => 400,
+        '_xs' => 120
+    ];
+
+    private function appendImageName($append)
+    {
+        $parts    = explode('.', $this->path);
+        $ext      = array_pop($parts);
+        $parts    = array_reverse($parts);
+        $parts[0] = $parts[0].$append;
+        $parts    = array_reverse($parts);
+        array_push($parts, $ext);
+        return implode('.', $parts);
+    }
+
+    private function createThumbnails()
+    {
+        $thumb = new Imagick($this->path);
+        foreach ($this->widths as $append => $width) {
+            $name = $this->appendImageName($append);
+            if ($this->size[0] > $width) {
+                $height = ($thumb->getImageHeight()/$thumb->getImageWidth())*$width;
+                $thumb->thumbnailImage($width, $height, true);
+            }
+            $thumb->writeImage($name);
+        }
+    }
+
+    private function fixOrientation()
+    {
+        if (isset($this->exif['Orientation'])) {
+            $orientation = $this->exif['Orientation'];
+        } elseif (isset($this->exif['IFD0']['Orientation'])) {
+            $orientation = $this->exif['IFD0']['Orientation'];
+        } else {
+
+            return false;
+        }
+
+        switch ($orientation) {
+            case 3:// rotate 180 degrees
+                $this->image->rotateimage("#FFF", 180);
+                break;
+
+            case 6:// rotate 90 degrees CW
+                $this->image->rotateimage("#FFF", 90);
+                break;
+
+            case 8:// rotate 90 degrees CCW
+                $this->image->rotateimage("#FFF", -90);
+                break;
+        }
+    }
+
+    private function optimize()
+    {
+        $this->image->setImageDepth(8);
+        $this->image->setCompressionQuality(60);
+        $this->image->setImageUnits(72);
+        $this->image->stripImage();
+    }
+
+    public function fire($job, $data = [])
+    {
+        $path = $data['path'];
+        if (($this->size = getimagesize($path)) === false) {
+            $job->delete();
+            return;
+        }
+        $this->path  = $path;
+        $this->exif  = exif_read_data($path);
+        $this->image = new Imagick($path);
+
+        /**
+         * Cleanup and fix original image orientation
+         */
+        $this->optimize();
+        $this->fixOrientation();
+        $this->image->writeImage($path);
+        $this->image->clear();
+
+        /**
+         * Create Thumbnails
+         */
+        $this->createThumbnails();
+
+        /**
+         * Cleanup memory
+         */
+        $job->delete();
+    }
+}
