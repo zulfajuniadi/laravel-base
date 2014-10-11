8467603240569a80d1af5683914a456bf2719967
Zulfa Juniadi Zulkifli <zulfajuniadi@gmail.com>
2014-10-12 00:19:58 +0800

    Enable database seeder to be run in databases other than mysql

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -9,15 +9,17 @@ class DatabaseSeeder extends Seeder {
      */
     public function run() {
         Eloquent::unguard();
-        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        try {
+            DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        } catch (Exception $e) {}
         $this->call('PermissionsTableSeeder');
         $this->call('RolesTableSeeder');
         $this->call('UsersTableSeeder');
         $this->call('OrganizationUnitsTableSeeder');
         $this->call('RoleUploadsTableSeeder');
-        DB::statement('SET FOREIGN_KEY_CHECKS=1');
-        // $this->call('');
-
+        try {
+            DB::statement('SET FOREIGN_KEY_CHECKS=1');
+        } catch (Exception $e) {}
     }
 
 }
