352c2fce0854a57027384e6ad934f0bb2457c0e4
Zulfa Juniadi Zulkifli <zulfajuniadi@gmail.com>
2014-10-11 12:43:29 +0800

    If Redirect->withErrors are called, the session is logged to ease debugging

--- app/boot.php
+++ app/boot.php
@@ -2,3 +2,9 @@
 
 Asset::push('js', 'application');
 Asset::push('css', 'application');
+if(Session::has('errors'))
+{
+    foreach(Session::get('errors')->all() as $message) {
+        Log::error($message);
+    }
+}
