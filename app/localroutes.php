917d64646609ee1f252531463a69d875ea56e75d
Zulfa Juniadi Zulkifli <zulfajuniadi@gmail.com>
2014-12-01 07:51:35 +0800

    Added /reset to local routes to call seeding

--- app/localroutes.php
+++ app/localroutes.php
@@ -5,3 +5,11 @@
  * useful for debugging or testing out new features
  * before moving it to the production environment
  */
+
+Route::get('/reset', function(){
+    Artisan::call('db:seed');
+    File::cleanDirectory(public_path() . '/uploads');
+    file_put_contents(public_path() . '/uploads/.gitignore', "*\n!.gitignore\n!index.html");
+    touch(public_path() . '/uploads/index.html');
+    return Redirect::action('AuthController@login');
+});
