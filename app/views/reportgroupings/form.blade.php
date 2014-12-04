83bb0967e88472d05122ead1482f62c2a0665a5a
Zulfa Juniadi Zulkifli <zulfajuniadi@gmail.com>
2014-12-04 11:37:30 +0800

    Help text for reports for better understanding

--- app/views/reportgroupings/form.blade.php
+++ app/views/reportgroupings/form.blade.php
@@ -1,14 +1,17 @@
 
 {{ Former::select('name')
     ->label('Field')
+    ->help('Which field to enable grouping with')
     ->options($report->availableFields(false))
     ->required() }}
 {{ Former::text('label')
+    ->help('Field label')
     ->label('Label')
     ->required() }}
-{{ Former::textarea('title_function')
-    ->label('Title Function') }}
+{{ Former::hidden('title_function')
+    ->value('') }}
 {{ Former::select('sql_function')
+    ->help('SQL aggregation function')
     ->placeholder('Choose One')
     ->options(ReportGrouping::$sqlFunctions)
     ->label('SQL Aggregate') }}
