d5d67956f941543c1ec478a2c3884d3391ddb99c
Zulfa Juniadi Zulkifli <zulfajuniadi@gmail.com>
2015-01-09 00:59:37 +0800

    renamed packages to frontend

---
+++ frontend/assets/javascripts/datatables.js
@@ -0,0 +1,11 @@
+//= require ../../packages/datatables/media/js/jquery.dataTables.js
+//= require ../../packages/datatables-bootstrap/dist/dataTables.bootstrap.js
+
+$('.DT').each(function(){
+  var target = $(this);
+  var path = target.data('path');
+  var DT = target.DataTable({
+    stateSave: true,
+    ajax: path
+  });
+});
