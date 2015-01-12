fb6ccee164dd7dbe447818d4d14006e1634d31ed
Zulfa Juniadi Zulkifli <zulfajuniadi@gmail.com>
2015-01-12 13:27:30 +0800

    Add form validation for browsers that does not support it. I'm looking at you safari. ಠ_ಠ

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,10 @@
 //= require ../packages/jquery/dist/jquery.js
 //= require ../packages/bootstrap/dist/js/bootstrap.js
 //= require ../packages/bootbox/bootbox.js
+//= require ../packages/better-dom/dist/better-dom.js
+//= require ../packages/better-i18n-plugin/dist/better-i18n-plugin.js
+//= require ../packages/better-popover-plugin/dist/better-popover-plugin.js
+//= require ../packages/better-form-validation/dist/better-form-validation.js
 
 ;(function(){
     $(document).on('click', '.confirm-delete', function(e){
---
+++ app/assets/javascripts/login.js
@@ -0,0 +1,4 @@
+//= require ../packages/better-dom/dist/better-dom.js
+//= require ../packages/better-i18n-plugin/dist/better-i18n-plugin.js
+//= require ../packages/better-popover-plugin/dist/better-popover-plugin.js
+//= require ../packages/better-form-validation/dist/better-form-validation.js
