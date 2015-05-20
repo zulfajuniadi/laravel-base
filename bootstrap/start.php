b066a33066f396f7ee0430f4f21f284acd34c503
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2015-05-21 00:54:46 +0800

    unset old hostname

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead', 'MacBookPro-B8F6B113A859-2.local'),
+	'local' => array('homestead', 'ZulfasMBP'),
 
 ));
 
