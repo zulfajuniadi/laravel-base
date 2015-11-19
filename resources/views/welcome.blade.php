5c5a21d077f0cde497f678103251cde504be2c1e
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2015-11-19 20:22:00 +0800

    Display inspiring quote on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="content">
-            <div class="title">Laravel 5</div>
+            <div class="title">{{Inspiring::quote()}}</div>
         </div>
     </div>
 @stop
