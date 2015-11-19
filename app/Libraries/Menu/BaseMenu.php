7d77c6f398443bdc6cebdaf92873b5d7eabbbba6
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2015-11-19 22:28:05 +0800

    Simplify menu generation (a bit)

--- app/Libraries/Menu/BaseMenu.php
+++ app/Libraries/Menu/BaseMenu.php
@@ -4,6 +4,12 @@ namespace App\Libraries\Menu;
 
 class BaseMenu
 {
+
+    public function check($action, $params = [])
+    {
+        return app('policy')->check($this->controller, $action, $params);
+    }
+
     public function __construct()
     {
         $this->menu = app('menu');
--- app/Menus/RolesMenu.php
+++ app/Menus/RolesMenu.php
@@ -3,11 +3,12 @@
 namespace App\Menus;
 
 use App\Libraries\Menu\BaseMenu;
+use App\Http\Controllers\RolesController;
 
 class RolesMenu extends BaseMenu
 {
 
-    protected $controller = 'App\Http\Controllers\RolesController';
+    protected $controller = RolesController::class;
 
     public function index($params)
     {
@@ -15,7 +16,7 @@ class RolesMenu extends BaseMenu
             ->addItem(action('RolesController@index'), trans('roles.roles'));
         $this->menu->handler('roles.panel-buttons')
             ->addClass('pull-right')
-            ->addItemIf(app('policy')->check($this->controller, 'create'), action('RolesController@create'), trans('roles.create'), 'btn btn-primary');
+            ->addItemIf($this->check('create'), action('RolesController@create'), trans('roles.create'), 'btn btn-primary');
     }
 
     public function create($params)
@@ -25,7 +26,7 @@ class RolesMenu extends BaseMenu
             ->addItem(action('RolesController@create'), trans('roles.create'));
         $this->menu->handler('roles.panel-buttons.create')
             ->addClass('pull-right')
-            ->addItemIf(app('policy')->check($this->controller, 'index'), action('RolesController@index'), trans('roles.list_all'), 'btn btn-primary');
+            ->addItemIf($this->check('index'), action('RolesController@index'), trans('roles.list_all'), 'btn btn-primary');
     }
 
     public function show($params)
@@ -35,13 +36,13 @@ class RolesMenu extends BaseMenu
             ->addItem(action('RolesController@show', $params['roles']->slug), $params['roles']->display_name);
         $this->menu->handler('roles.panel-buttons.show')
             ->addClass('pull-right')
-            ->addItemIf(app('policy')->check($this->controller, 'index'), action('RolesController@index'), trans('roles.list_all'), 'btn btn-primary')
-            ->addItemIf(app('policy')->check($this->controller, 'create'), action('RolesController@create'), trans('roles.create'), 'btn btn-default');
+            ->addItemIf($this->check('index'), action('RolesController@index'), trans('roles.list_all'), 'btn btn-primary')
+            ->addItemIf($this->check('create'), action('RolesController@create'), trans('roles.create'), 'btn btn-default');
         $this->menu->handler('roles.record-buttons.show')
-            ->addItemIf(app('policy')->check($this->controller, 'edit', $params), action('RolesController@edit', $params['roles']->slug), trans('roles.edit'), 'btn btn-primary')
-            ->addItemIf(app('policy')->check($this->controller, 'revisions', $params), action('RolesController@revisions', $params['roles']->slug), trans('roles.revisions'), 'btn btn-default')
-            ->addItemIf(app('policy')->check($this->controller, 'duplicate', $params), action('RolesController@duplicate', $params['roles']->slug),  trans('roles.duplicate'), 'btn btn-default')
-            ->addItemIf(app('policy')->check($this->controller, 'delete', $params), action('RolesController@delete', $params['roles']->slug), trans('roles.delete'), 'btn btn-danger confirm-action');
+            ->addItemIf($this->check('edit', $params), action('RolesController@edit', $params['roles']->slug), trans('roles.edit'), 'btn btn-primary')
+            ->addItemIf($this->check('revisions', $params), action('RolesController@revisions', $params['roles']->slug), trans('roles.revisions'), 'btn btn-default')
+            ->addItemIf($this->check('duplicate', $params), action('RolesController@duplicate', $params['roles']->slug),  trans('roles.duplicate'), 'btn btn-default')
+            ->addItemIf($this->check('delete', $params), action('RolesController@delete', $params['roles']->slug), trans('roles.delete'), 'btn btn-danger confirm-action');
     }
 
     public function edit($params)
@@ -51,9 +52,9 @@ class RolesMenu extends BaseMenu
             ->addItem(action('RolesController@show', $params['roles']->slug), $params['roles']->display_name);
         $this->menu->handler('roles.panel-buttons.edit')
             ->addClass('pull-right')
-            ->addItemIf(app('policy')->check($this->controller, 'index'), action('RolesController@index'), trans('roles.list_all'), 'btn btn-primary');
+            ->addItemIf($this->check('index'), action('RolesController@index'), trans('roles.list_all'), 'btn btn-primary');
         $this->menu->handler('roles.record-buttons.edit')
-            ->addItemIf(app('policy')->check($this->controller, 'show'), action('RolesController@show', $params['roles']->slug), trans('roles.show'), 'btn btn-default');
+            ->addItemIf($this->check('show'), action('RolesController@show', $params['roles']->slug), trans('roles.show'), 'btn btn-default');
     }
 
     public function revisions($params)
@@ -61,18 +62,18 @@ class RolesMenu extends BaseMenu
         $this->menu->handler('breadcrumbs')
             ->addItem(action('RolesController@index'), trans('roles.roles'))
             ->addItem(action('RolesController@show', $params['roles']->slug), $params['roles']->display_name)
-            ->addItem(action('RolesController@revisions', $params['roles']->slug), 'Revisions');
+            ->addItem(action('RolesController@revisions', $params['roles']->slug), trans('roles.revisions'));
         $this->menu->handler('roles.panel-buttons.revisions')
             ->addClass('pull-right')
-            ->addItemIf(app('policy')->check($this->controller, 'index'), action('RolesController@index'), trans('roles.list_all'), 'btn btn-primary')
-            ->addItemIf(app('policy')->check($this->controller, 'show'), action('RolesController@show', $params['roles']->slug), trans('roles.show'), 'btn btn-default');
+            ->addItemIf($this->check('index'), action('RolesController@index'), trans('roles.list_all'), 'btn btn-primary')
+            ->addItemIf($this->check('show'), action('RolesController@show', $params['roles']->slug), trans('roles.show'), 'btn btn-default');
     }
 
     public function __construct()
     {
         parent::__construct();
         $this->menu->handler('admin')
-            ->addItemIf(app('policy')->check($this->controller, 'index'), action('RolesController@index'), trans('roles.roles'));
+            ->addItemIf($this->check('index'), action('RolesController@index'), trans('roles.roles'));
     }
 
 }
