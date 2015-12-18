a2d24fc0ae829e78a9e0c1b5364707813ad73584
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2015-12-18 20:14:10 +0800

    Ability to set user's password on create

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -18,6 +18,10 @@
                         ->label('roles.roles')
                         ->multiple()
                         ->options(App\Role::options()) !!}
+                    {!! Former::text('password')
+                        ->value(str_random(12))
+                        ->label('users.password')
+                        ->required() !!}
                 </div>
                 <div class="panel-footer">
                     <button class="btn btn-primary">{{trans('users.submit')}}</button>
