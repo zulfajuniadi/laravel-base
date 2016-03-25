9ca290d0e9adbbe5a53ebf70420abdcc1d0892fa
Muhammad Syafiq <syafiq@my-sands.com>
2016-03-25 08:28:32 +0800

    Upgrade to Laravel 5.2. .env.example: CACHE_DRIVE change to array due to zizaco/entrust's 'this cache store does not support tagging' error if using file. composer.json: change zizaco/entrust version to 5.2. composer.json: add symphony/css-selector & symphony/dom-crawler.

--- config/app.php
+++ config/app.php
@@ -7,6 +7,20 @@ return [
      */
     'name' => 'Laravel Base',
 
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Environment
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the "environment" your application is currently
+    | running in. This may determine how you prefer to configure various
+    | services your application utilizes. Set this in your ".env" file.
+    |
+    */
+
+    'env' => env('APP_ENV', 'local'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Debug Mode
@@ -124,7 +138,6 @@ return [
         Illuminate\Bus\BusServiceProvider::class,
         Illuminate\Cache\CacheServiceProvider::class,
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
-        Illuminate\Routing\ControllerServiceProvider::class,
         Illuminate\Cookie\CookieServiceProvider::class,
         Illuminate\Database\DatabaseServiceProvider::class,
         Illuminate\Encryption\EncryptionServiceProvider::class,
